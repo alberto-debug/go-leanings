98a93342f1516debb6ec01d53414d5a3bfa377eb
Alberto <albertojunior848423803@gmail.com>
2025-03-29 23:54:12 +0300

    changes

--- internal/handler/todo.go
+++ internal/handler/todo.go
@@ -8,3 +8,9 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("Hello World")
 }
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Server running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
