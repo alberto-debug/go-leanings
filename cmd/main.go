1a4b539ad5c12960f836e2e539eea4a26875443d
Alberto <albertojunior848423803@gmail.com>
2025-03-30 19:48:16 +0300

    changes

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,1 @@
 package main
-
-import (
-	"fmt"
-	"net/http"
-)
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello world")
-}
-
-func main() {
-	http.HandleFunc("/", handler)
-	fmt.Println("Server running on http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
-}
