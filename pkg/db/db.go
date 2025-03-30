59b91af83cfddd3671f6b4a5c3351e84aeeeeede
Alberto <albertojunior848423803@gmail.com>
2025-03-30 21:28:21 +0300

    changes

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,6 +1,9 @@
 package db
 
-import "database/sql"
+import (
+	"database/sql"
+	"log"
+)
 
 var DB *sql.DB
 
@@ -8,4 +11,24 @@ func initDB() {
 	var err error
 
 	DB, err = sql.Open("mysql", "username:password@tcp(127.0.0.1:3306)/todos")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := DB.Ping(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Createtable() {
+	query := `
+    CREATE TABLE IF NOT EXISTS todos (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        completed BOOLEAN NOT NULL DEFAULT 0
+    );`
+	_, err := DB.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
