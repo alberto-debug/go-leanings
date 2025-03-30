41f0cb32676c2a1e8a00d85bd7259b5f82133ebd
Alberto <albertojunior848423803@gmail.com>
2025-03-30 22:53:09 +0300

    changes

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,7 @@
 package main
 
-import (
-	"log"
-	"net/http"
+import "net/http"
 
-	"github.com/alberto-debug/todo-backend/internal/handlers"
-	"github.com/alberto-debug/todo-backend/pkg/db"
-)
-
-func main() {
-	db.InitDB()
-	http.HandleFunc("/api/todos", handlers.TodoHandler)
-	log.Println("Server started at :8080, test qand enjoy your app")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.println(w, "Hello World")
 }
