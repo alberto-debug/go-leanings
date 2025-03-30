b32facc9af554211d17c1676e1d1f063f0928f84
Alberto <albertojunior848423803@gmail.com>
2025-03-30 22:23:22 +0300

    changes

--- cmd/main.go
+++ cmd/main.go
@@ -11,6 +11,6 @@ import (
 func main() {
 	db.InitDB()
 	http.HandleFunc("/api/todos", handlers.TodoHandler)
-	log.Println("Server started at :8080")
+	log.Println("Server started at :8080, test qand enjoy your app")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
